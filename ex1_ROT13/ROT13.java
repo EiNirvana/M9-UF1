d8aeb1af0a3706e767b7adc8d1c10b6b34749014
Gisela Castejón Benito <144903972+EiNirvana@users.noreply.github.com>
2024-09-26 18:55:22 +0200

    final de ROT13 i començament de ROTX

---
+++ ex1_ROT13/ROT13.java
@@ -0,0 +1,108 @@
+package ex1_ROT13;
+/**
+ * Activitat 1: Criptorització d'un missatge per part de ROT-13, a més del procès invers.
+ */
+
+public class ROT13 {
+    public static char[] MIN ={'a', 'à', 'b', 'c', 'ç', 'd', 'e','è', 'é', 'f', 'g', 'h', 'i', 'í', 'j', 'k', 'l',
+     'm', 'n', 'ñ', 'o', 'ò', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'v', 'w', 'x', 'y', 'z'};
+    public static char[] MAJ = {'A', 'À', 'B', 'C', 'Ç', 'D', 'E', 'È', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L',
+     'M', 'N', 'Ñ', 'O', 'Ò', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'V', 'W', 'X', 'Y', 'Z'};
+
+    public static void main(String[] args){
+        String originalMessage = "La Conservació I El Tracte Correcte Del Mediambient És Important Per A La Salut.";
+        String finalMessageEncriptat = "";
+        String finalMessageDesencriptat = "";
+                    
+        finalMessageEncriptat = xifratROT13(originalMessage);
+        finalMessageDesencriptat = desxifratROT13(originalMessage);
+
+        System.out.println("Missatge original: " + originalMessage);
+        System.out.println("Missatge encriptat: " + finalMessageEncriptat);
+        System.out.println("Missatge desencriptat: " + finalMessageDesencriptat); //S'ha usat el mateix missatge en la desencriptació, el text no a de tenir sentit
+    }
+
+    public static String xifratROT13(String missatgeOriginal){
+        String missatgeFinal = "";
+        char caracFinal= ' ';
+        char caracLlista;
+        int num;
+        int llargada = missatgeOriginal.length();
+        for (int i = 0; i < llargada; i++){
+            num = 13;
+            char caracAnal = missatgeOriginal.charAt(i);
+            if (Character.isLetter(caracAnal)){
+                if (Character.isUpperCase(caracAnal)){
+                    for (int p = 0; p < MAJ.length; p ++){
+                        caracLlista = MAJ[p];
+                        if (caracLlista == caracAnal){
+                            num += p; 
+                            if (num >= MAJ.length){
+                                num = num - MAJ.length;
+                                caracFinal = MAJ[num];
+                            } else {caracFinal = MAJ[num];}
+                            missatgeFinal += caracFinal;
+                        } else continue;
+                    }
+                } else {
+                    for (int p = 0; p < MIN.length; p++){
+                        caracLlista = MIN[p];
+                        if (caracLlista == caracAnal){
+                            num += p;
+                            if (num >= MIN.length){
+                                num = num - MIN.length;
+                                caracFinal = MIN[num];
+                            } else {caracFinal = MIN[num];}
+                            missatgeFinal += caracFinal;
+                        } else continue;
+                    }
+                }
+            } else {missatgeFinal += caracAnal;}
+        }
+        if (missatgeFinal.isEmpty()){missatgeFinal = "?";}
+        return missatgeFinal;
+    }
+    public static String desxifratROT13(String missatgeOriginal){
+        String missatgeFinal = "";
+        char caracFinal= ' ';
+        char caracLlista;
+        int num;
+        int numFinal;
+        int llargada = missatgeOriginal.length();
+        for (int i = 0; i < llargada; i++){
+            num = 13;
+            numFinal = 0;
+            char caracAnal = missatgeOriginal.charAt(i);
+            if (Character.isLetter(caracAnal)){
+                if (Character.isUpperCase(caracAnal)){
+                    for (int p = 0; p < MAJ.length; p ++){
+                        caracLlista = MAJ[p];
+                        if (caracLlista == caracAnal){
+                            numFinal = p - num; 
+                            if (numFinal < 0){
+                                numFinal = numFinal + MAJ.length;
+                                caracFinal = MAJ[numFinal];
+                            } else {caracFinal = MAJ[numFinal];}
+                            missatgeFinal += caracFinal;
+                        } else continue;
+                    }
+                } else {
+                    for (int p = 0; p < MIN.length; p++){
+                        caracLlista = MIN[p];
+                        if (caracLlista == caracAnal){
+                            numFinal = p - num; 
+                            if (numFinal < 0){
+                                numFinal = numFinal + MIN.length;
+                                caracFinal = MIN[numFinal];
+                            } else {caracFinal = MIN[numFinal];}
+                            missatgeFinal += caracFinal;
+                        } else continue;
+                    }
+                }
+            } else {missatgeFinal += caracAnal;}
+        }
+        if (missatgeFinal.isEmpty()){missatgeFinal = "?";}
+        return missatgeFinal;
+    }
+        
+}
--- ex2_ROTX/ROTX.java
+++ ex2_ROTX/ROTX.java
@@ -1,35 +1,50 @@
 /**
- * Activitat 1: Criptorització d'un missatge per part de ROT-13, a més del procès invers.
+ * Exercici següent a ROT13, en que ara l'usuari escull quin es el núm de la codificació. 
+ * A més a més, hi ha una pàgina que descodifica el codi a la força.
  */
 
-public class ROT13 {
-    public static char[] MIN ={'a', 'à', 'b', 'c', 'ç', 'd', 'e','è', 'é', 'f', 'g', 'h', 'i', 'í', 'j', 'k', 'l',
+package ex2_ROTX;
+
+public class ROTX {
+    public static char[] MIN ={'a', 'à', 'á', 'b', 'c', 'ç', 'd', 'e','è', 'é', 'f', 'g', 'h', 'i', 'í', 'j', 'k', 'l',
      'm', 'n', 'ñ', 'o', 'ò', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'v', 'w', 'x', 'y', 'z'};
     public static char[] MAJ = {'A', 'À', 'B', 'C', 'Ç', 'D', 'E', 'È', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L',
      'M', 'N', 'Ñ', 'O', 'Ò', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'V', 'W', 'X', 'Y', 'Z'};
 
     public static void main(String[] args){
-        String originalMessage = "The quick fox jumps over the angry dog";
         String finalMessageEncriptat = "";
         String finalMessageDesencriptat = "";
-                    
-        finalMessageEncriptat = xifratROT13(originalMessage);
-        finalMessageDesencriptat = desxifratROT13(originalMessage);
+        int[] numProves = {3, 15, 5, 9, 8};
+        String[] numOriginalMessage = {
+            "The quick fox jumps over the angry dog", 
+            "La zebra busca el seu calçat",
+            "A Rafael le ha venido una ñoña de tres al cuarto",
+            "Busca el que és, i el que no és, no serà pas.",
+            "La Conservació I El Tracte Correcte Del Mediambient És Important Per A La Salut."
+        };
+        
+        for (int i = 0; i < numProves.length; i++){
+            int desplaçament = numProves[i];
+            String originalMessage = numOriginalMessage[i];
 
-        System.out.println("Missatge original: " + originalMessage);
-        System.out.println("Missatge encriptat: " + finalMessageEncriptat);
-        System.out.println("Missatge desencriptat: " + finalMessageDesencriptat); //S'ha usat el mateix missatge en la desencriptació, el text no a de tenir sentit
+            finalMessageEncriptat = xifratROTX(originalMessage, desplaçament);
+            finalMessageDesencriptat = desxifratROTX(originalMessage, desplaçament);
+
+            System.out.println("Missatge original: " + originalMessage);
+            System.out.println("Missatge encriptat: " + finalMessageEncriptat);
+            System.out.println("Missatge desencriptat: " + finalMessageDesencriptat); //S'ha usat el mateix missatge en la desencriptació, el text no a de tenir sentit
+        }
     }
 
-    public static String xifratROT13(String missatgeOriginal){
+    public static String xifratROTX(String cadena, int desplaçament){
         String missatgeFinal = "";
         char caracFinal= ' ';
         char caracLlista;
         int num;
-        int llargada = missatgeOriginal.length();
+        int llargada = cadena.length();
         for (int i = 0; i < llargada; i++){
-            num = 13;
-            char caracAnal = missatgeOriginal.charAt(i);
+            num = desplaçament;
+            char caracAnal = cadena.charAt(i);
             if (Character.isLetter(caracAnal)){
                 if (Character.isUpperCase(caracAnal)){
                     for (int p = 0; p < MAJ.length; p ++){
@@ -61,17 +76,17 @@ public class ROT13 {
         if (missatgeFinal.isEmpty()){missatgeFinal = "?";}
         return missatgeFinal;
     }
-    public static String desxifratROT13(String missatgeOriginal){
+    public static String desxifratROTX(String cadena, int desplaçament){
         String missatgeFinal = "";
         char caracFinal= ' ';
         char caracLlista;
         int num;
         int numFinal;
-        int llargada = missatgeOriginal.length();
+        int llargada = cadena.length();
         for (int i = 0; i < llargada; i++){
-            num = 13;
+            num = desplaçament;
             numFinal = 0;
-            char caracAnal = missatgeOriginal.charAt(i);
+            char caracAnal = cadena.charAt(i);
             if (Character.isLetter(caracAnal)){
                 if (Character.isUpperCase(caracAnal)){
                     for (int p = 0; p < MAJ.length; p ++){
@@ -103,5 +118,8 @@ public class ROT13 {
         if (missatgeFinal.isEmpty()){missatgeFinal = "?";}
         return missatgeFinal;
     }
-        
+
+    //public static String desxifratROT(String cadenaXifrada){
+
+    //}
 }
