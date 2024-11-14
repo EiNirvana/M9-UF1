2fe79a4f7ead60c6183397dc8d44c9800e9bc8c6
Gisela Castejón Benito <144903972+EiNirvana@users.noreply.github.com>
2024-11-14 19:00:06 +0100

    Final de classe

--- 08-Hashes/Hashes.java
+++ 08-Hashes/Hashes.java
@@ -5,11 +5,19 @@
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HexFormat;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import java.io.IOException;
 
 public class Hashes {
     char charset = "abcdefABCDEF1234567890!".charAt();
     public int npass = 0;
+    private static final int ITERATION = 10000; 
+    private static final int HASH_SIZE = 16;
 
     public static void main(String[] args) throws Exception {
         String salt = "qpoweiruañslkdfjz";
@@ -57,13 +65,77 @@ public class Hashes {
         }
         return finalPass;
     }
+
     public String getPBKDF2AmbSalt(String pw, String salt){
+        try{
+            byte[] saltByte = salt.getBytes(StandardCharsets.UTF_8);
+            PBEKeySpec spec = new PBEKeySpec(pw.toCharArray(), saltByte, ITERATION, HASH_SIZE * 8);
+            SecretKeyFactory skPB = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hash = skPB.generateSecret(spec).getEncoded();
+            HexFormat hex = HexFormat.of();
+            hash = hex.formatHex(bytes);
 
+        } catch (IOException p){
+            System.out.println("ERROR: Something went wrong when creating the PBKDF2.");
+            p.printStackTrace();
+        }
+        return toHex(hash);
     }
+
+    /*  necessary for the modul atop this one.
+    private static String toHex(byte[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            sb.append(Integer.toString((array[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
+    }
+    */
+
     public String forcaBruta(String alg, String hash, String salt){
+        /**
+         * for (---)
+         * if ((testPw (0-----) != null) return pw
+         * for (---)
+         * testPw (-0----)
+         * for ...
+         */
+        try{
+            for (int length = 1; length <= 6; length++) {
+                // Generem totes les combinacions possibles de longitud actual
+                char[] password = new char[length];
+                if ((testPw(password, 0, length, alg, targetHash, salt)) != null) {
+                    return new String(password);
+                }
+            }
+            return null;
 
+        } catch (IOException q){
+            System.out.println("There has been a mistake when executing forcaBruta");
+            q.printStackTrace();
+        }
     }
-    public String getInterval(long t1, long t2){
 
+    private boolean testPw()
+
+
+    public String getInterval(long t1, long t2){
+        long duration = t2 - t1;
+
+        long seconds = duration / 1000;
+        seconds = seconds % 60;
+
+        long millis = duration % 1000;
+
+        long minutes = seconds / 60;
+        minutes = minutes % 60;
+
+        long hours = minutes / 60;
+        hours = hours % 24;
+
+        long days = hours / 24;
+        
+        return String.format("%d dies / %d hores / %d minuts / %d segons / %d millis", 
+                days, hours, minutes, seconds, millis);
     }
 }
